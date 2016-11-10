29c8a8bd1278d2388f3a865d9b6c59de24838d2a
Jon Friskics <jonfriskics@gmail.com>
2016-11-10 17:17:04 -0500

    first attempt

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
   // your code will go here
+// https://www.codeschool.com/users/110833.json
 
+  $.ajax({
+    url: 'https://www.codeschool.com/users/110833.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      $.each(response.courses.completed, function(c) {
+        var courseDiv = $('div').addClass('course');
+        courseDiv.append('h3').text(c.title);
+        courseDiv.append('img').attr('src',c.badge);
+        courseDiv.append('a').attr({'href':c.url,'target':'_blank'}).addClass('btn').addClass('btn-primary').text('See Course');
+        $('#badges').append('div');
+
+      })
+    }
+  })
 });
